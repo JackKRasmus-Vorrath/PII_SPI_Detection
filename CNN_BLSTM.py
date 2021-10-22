341b80f398966066e161a42f03f895a06ae2b229
Dr. Jack K. Rasmus-Vorrath <jkrasmus@alumni.harvard.edu>
2021-10-22 12:44:12 -0700

    Update CNN_BLSTM.py
    formatting

--- CNN_BLSTM.py
+++ CNN_BLSTM.py
@@ -37,12 +37,34 @@ from keras.layers import Masking, MaxPooling1D, SpatialDropout1D, BatchNormaliza
 def get_args():
 
 	parser = PromptParser()
-	parser.add_argument("-D", "--Data", default="./Data/", type=str, help="Data Directory", prompt=True)
-	parser.add_argument("-M", "--Models", default="./Models/", type=str, help="Models Directory", prompt=True)
-	parser.add_argument("-T", "--Train", default=True, type=str, help="Train New Model", prompt=True)
-	parser.add_argument("-P", "--Predict", default=True, type=str, help="Predict using Trained Model", prompt=True)
-	parser.add_argument("-L", "--loadedModel", default="CNN_BLSTM.h5", help="Pretrained Model Path", prompt=True)
-	parser.add_argument("-G", "--Debug", default=False, help="Debug Mode", prompt=False)
+	parser.add_argument("-D", "--Data", 
+			    default="./Data/", 
+			    type=str, 
+			    help="Data Directory", 
+			    prompt=True)
+	parser.add_argument("-M", "--Models", 
+			    default="./Models/", 
+			    type=str, 
+			    help="Models Directory", 
+			    prompt=True)
+	parser.add_argument("-T", "--Train", 
+			    default=True, 
+			    type=str, 
+			    help="Train New Model", 
+			    prompt=True)
+	parser.add_argument("-P", "--Predict", 
+			    default=True, 
+			    type=str, 
+			    help="Predict using Trained Model", 
+			    prompt=True)
+	parser.add_argument("-L", "--loadedModel", 
+			    default="CNN_BLSTM.h5",
+			    help="Pretrained Model Path", 
+			    prompt=True)
+	parser.add_argument("-G", "--Debug", 
+			    default=False, 
+			    help="Debug Mode", 
+			    prompt=False)
 
 	args = parser.parse_args()
 	
@@ -51,7 +73,10 @@ def get_args():
 def read_data(args):
 	
 	csv_file = '{}training.csv'.format(args.Data)
-	dataframe = pd.read_csv(csv_file, engine='python', quotechar='|', header=None)
+	dataframe = pd.read_csv(csv_file, 
+				engine='python', 
+				quotechar='|', 
+				header=None)
 	dataset = dataframe.sample(frac=1).values
 	
 	X = dataset[:,0]
@@ -59,54 +84,76 @@ def read_data(args):
 
 	for index, item in enumerate(X):
 		
-		reqJson = json.loads(item, object_pairs_hook=OrderedDict)
-		X[index] = json.dumps(reqJson, separators=(',', ':'))
+		reqJson = json.loads(item, 
+				     object_pairs_hook=OrderedDict)
+		X[index] = json.dumps(reqJson, 
+				      separators=(',', ':'))
 		
 	return X, y
 	
-def preprocess(X, to_tokenize):
+def preprocess(X, 
+	       to_tokenize):
 
 	to_exclude = '!"#%&()*+,/:;<=>?@[\\]^_`|{}~\t\n'
 	
-	tokenizer = Tokenizer(filters=to_exclude, char_level=True, lower=False, oov_token='')
-	X_clean = [re.sub(r'(['+to_tokenize+'])', r' ', x) for x in X]
+	tokenizer = Tokenizer(filters=to_exclude, 
+			      char_level=True, 
+			      lower=False, 
+			      oov_token='')
+	X_clean = [re.sub(r'(['+to_tokenize+'])', r' ', x) 
+		   	for x in X]
 	
 	tokenizer.fit_on_texts(X_clean)
 	
 	X_seq = tokenizer.texts_to_sequences(X_clean)
 	
 	n_unique = len(tokenizer.word_index)+1
-	payload_len = max([len(x) for x in X_seq])
+	payload_len = max([len(x) 
+			   	for x in X_seq])
 	
-	X_processed = pad_sequences(X_seq, maxlen=payload_len, padding='post', truncating='post')
+	X_processed = pad_sequences(X_seq, 
+				    maxlen=payload_len, 
+				    padding='post', 
+				    truncating='post')
 	
 	return X_processed, n_unique, payload_len, tokenizer
 	
-def build_model(n_unique, payload_len):
+def build_model(n_unique, 
+		payload_len):
 	
 	#Padded Input
 	char_in = Input(shape=(payload_len,))
 	
 	#Embed Characters
-	emb_char = Embedding(input_dim=n_unique, output_dim=16, 
-							   input_length=payload_len)(char_in)
+	emb_char = Embedding(input_dim=n_unique, 
+			     output_dim=16, 
+			     input_length=payload_len)(char_in)
 
 	#1D Convolution + Pooling
-	conv1 = Conv1D(kernel_size=4, filters=16, padding='same', activation='relu', strides=1)(emb_char)
+	conv1 = Conv1D(kernel_size=4, 
+		       filters=16, 
+		       padding='same', 
+		       activation='relu', 
+		       strides=1)(emb_char)
 	pool = MaxPooling1D(strides=2)(conv1)
 	
 	mask = Masking(mask_value=0)(pool)
 
 	#LSTM Hidden Layer
-	char_enc = LSTM(units=32, return_sequences=True, dropout=0.5,
-									recurrent_dropout=0.5)(mask)
+	char_enc = LSTM(units=32, 
+			return_sequences=True, 
+			dropout=0.5,
+			recurrent_dropout=0.5)(mask)
 
 	#1D Spatial Dropout + BLSTM
 	x = SpatialDropout1D(0.5)(char_enc)
-	main_lstm = Bidirectional(LSTM(units=32, return_sequences=True, dropout=0.5,
-								   recurrent_dropout=0.5))(x)
+	main_lstm = Bidirectional(LSTM(units=32, 
+				       return_sequences=True, 
+				       dropout=0.5,
+				       recurrent_dropout=0.5))(x)
 	#LSTM Stacked
-	out_lstm = LSTM(units=16, return_sequences=False)(main_lstm)
+	out_lstm = LSTM(units=16, 
+			return_sequences=False)(main_lstm)
 
 	#Batch Normalization
 	bn = BatchNormalization()(out_lstm)
@@ -116,19 +163,29 @@ def build_model(n_unique, payload_len):
 
 	model = Model([char_in], out)
 	
-	model.compile(optimizer="rmsprop", loss="binary_crossentropy", metrics=["acc"])
+	model.compile(optimizer="rmsprop", 
+		      loss="binary_crossentropy",
+		      metrics=["acc"])
 	
 	print(model.summary())
 	
 	return model
 	
-def prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len):
+def prediction(loaded_model, 
+	       val_input, 
+	       to_tokenize, 
+	       tokenizer, 
+	       payload_len):
 	
-	val_input_clean = [re.sub(r'(['+to_tokenize+'])', r' ', x) for x in [val_input]]
+	val_input_clean = [re.sub(r'(['+to_tokenize+'])', r' ', x) 
+			   	for x in [val_input]]
 	
 	X_val_seq = tokenizer.texts_to_sequences(val_input_clean)
 
-	X_val_processed = pad_sequences(X_val_seq, maxlen=payload_len, padding='post', truncating='post')
+	X_val_processed = pad_sequences(X_val_seq, 
+					maxlen=payload_len, 
+					padding='post', 
+					truncating='post')
 	
 	res = loaded_model.predict(np.array(X_val_processed))
 	
@@ -146,7 +203,10 @@ def main():
 	
 	X_processed, n_unique, payload_len, tokenizer = preprocess(X, to_tokenize)
 	
-	X_train, X_test, y_train, y_test = train_test_split(X_processed, y, test_size=0.2, random_state=222)
+	X_train, X_test, y_train, y_test = train_test_split(X_processed, 
+							    y, 
+							    test_size=0.2,
+							    random_state=222)
 	
 	print('~'*80)
 	print(f'\nShape of Training Set is: {X_train.shape}')
@@ -158,23 +218,36 @@ def main():
 	
 	if args.Train not in ['False','false','f','F']:
 	
-		model = build_model(n_unique, payload_len)
+		model = build_model(n_unique, 
+				    payload_len)
 		
 		###################################################
 		#TRAINING##########################################
 		
-		early_stop = EarlyStopping(monitor='val_loss', patience=2)
-		checkpoint = ModelCheckpoint(filepath='./CNN_BLSTM_Checkpoint.hdf5', monitor='val_loss', verbose=1, save_best_only=True)
+		early_stop = EarlyStopping(monitor='val_loss', 
+					   patience=2)
+		checkpoint = ModelCheckpoint(filepath='./CNN_BLSTM_Checkpoint.hdf5', 
+					     monitor='val_loss', 
+					     verbose=1, 
+					     save_best_only=True)
 		callbacks_list = [checkpoint, early_stop]
 		
-		history = model.fit([X_train], y_train, batch_size=32, epochs=10, validation_split=0.2, callbacks=callbacks_list, verbose=1)
+		history = model.fit([X_train], 
+				    y_train, 
+				    batch_size=32, 
+				    epochs=10, 
+				    validation_split=0.2, 
+				    callbacks=callbacks_list,
+				    verbose=1)
 		
 		model.save_weights(f'{args.Models}CNN_BLSTM.h5')
 		model.save(f'{args.Models}CNN_BLSTM.h5')
 		with open(f'{args.Models}CNN_BLSTM.json', 'w') as outfile:
 			outfile.write(model.to_json())
 		
-		score, acc = model.evaluate([np.array(X_test)], np.array(y_test).reshape(len(y_test), 1), batch_size=32)
+		score, acc = model.evaluate([np.array(X_test)], 
+					    np.array(y_test).reshape(len(y_test), 1), 
+					    batch_size=32)
 		
 		print(f'Trained Model Performance on {len(X_test)} Documents of Hold-out Test Data:\n')
 		print(f'Loss: {score} | Accuracy: {acc}')
@@ -186,7 +259,9 @@ def main():
 	
 		loaded_model = load_model(f'{args.Models}{args.loadedModel}')
 		
-		score, acc = loaded_model.evaluate([np.array(X_test)], np.array(y_test).reshape(len(y_test), 1), batch_size=32)
+		score, acc = loaded_model.evaluate([np.array(X_test)], 
+						   np.array(y_test).reshape(len(y_test), 1), 
+						   batch_size=32)
 		
 		print(f'\nPretrained Model Performance on {len(X_test)} Documents of Hold-out Test Data:\n')
 		print(f'Loss: {score} | Accuracy: {acc}\n')
@@ -195,91 +270,143 @@ def main():
 		
 		print('~'*80)	
 		val_input = ''' {"userid":"John Smith","password":"mypassword123","my_location":"123 S. Street","social":"123-45-6789"} '''		
-		res =  prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len)		
+		res =  prediction(loaded_model, 
+				  val_input, 
+				  to_tokenize, 
+				  tokenizer, 
+				  payload_len)		
 		print(f'Sensitive Document Payload: {val_input}\n')
 		print(f'Probability that document is sensitive: {int(res[0][0]*100)}%')		
 		####################################################
 		
 		print('~'*80)		
 		val_input = ''' {"user":"Ramona Schwarz","marital":"S","workflow":"sfYeowi09"} '''		
-		res = prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len)		
+		res = prediction(loaded_model, 
+				 val_input, 
+				 to_tokenize, 
+				 tokenizer, 
+				 payload_len)		
 		print(f'Sensitive Document Payload: {val_input}\n')
 		print(f'Probability that document is sensitive: {int(res[0][0]*100)}%')
 		####################################################
 		
 		print('~'*80)		
 		val_input = ''' {"resource":"175.0.1.45","location":"NYC","workflow":"sfYeowi09"} '''		
-		res = prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len)		
+		res = prediction(loaded_model, 
+				 val_input, 
+				 to_tokenize, 
+				 tokenizer, 
+				 payload_len)		
 		print(f'Sensitive Document Payload: {val_input}\n')
 		print(f'Probability that document is sensitive: {int(res[0][0]*100)}%')
 		####################################################
 		
 		print('~'*80)		
 		val_input = ''' {"resource":"gHOSkiQF","location":"(40.71, 71.00)","workflow":"sfYeowi09"} '''		
-		res = prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len)		
+		res = prediction(loaded_model, 
+				 val_input, 
+				 to_tokenize, 
+				 tokenizer, 
+				 payload_len)		
 		print(f'Sensitive Document Payload: {val_input}\n')
 		print(f'Probability that document is sensitive: {int(res[0][0]*100)}%')
 		####################################################
 		
 		print('~'*80)		
 		val_input = ''' {"resource":"gHOSkiQF","location":"tvHqbbjY","birthdate":"01-01-1970"} '''		
-		res = prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len)		
+		res = prediction(loaded_model, 
+				 val_input, 
+				 to_tokenize, 
+				 tokenizer, 
+				 payload_len)		
 		print(f'Sensitive Document Payload: {val_input}\n')
 		print(f'Probability that document is sensitive: {int(res[0][0]*100)}%')
 		####################################################
 		
 		print('~'*80)		
 		val_input = ''' {"gender":"F","location":"Los Angeles","workflow":"sfYeowi09"} '''		
-		res = prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len)		
+		res = prediction(loaded_model, 
+				 val_input, 
+				 to_tokenize, 
+				 tokenizer, 
+				 payload_len)		
 		print(f'Sensitive Document Payload: {val_input}\n')
 		print(f'Probability that document is sensitive: {int(res[0][0]*100)}%')
 		####################################################
 		
 		print('~'*80)		
 		val_input = ''' {"AGI":"250000","location":"tvHqbbjY","workflow":"sfYeowi09"} '''		
-		res = prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len)		
+		res = prediction(loaded_model, 
+				 val_input, 
+				 to_tokenize, 
+				 tokenizer, 
+				 payload_len)		
 		print(f'Sensitive Document Payload: {val_input}\n')
 		print(f'Probability that document is sensitive: {int(res[0][0]*100)}%')
 		####################################################
 		
 		print('~'*80)		
 		val_input = ''' {"CC#":"5234 7466 8967 3458","location":"tvHqbbjY","workflow":"sfYeowi09"} '''		
-		res = prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len)		
+		res = prediction(loaded_model, 
+				 val_input, 
+				 to_tokenize, 
+				 tokenizer, 
+				 payload_len)		
 		print(f'Sensitive Document Payload: {val_input}\n')
 		print(f'Probability that document is sensitive: {int(res[0][0]*100)}%')
 		####################################################
 		
 		print('~'*80)		
 		val_input = ''' {"AGI":"XXXXXX","location":"XX","workflow":"XXXXX"} '''		
-		res = prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len)		
+		res = prediction(loaded_model, 
+				 val_input, 
+				 to_tokenize, 
+				 tokenizer, 
+				 payload_len)		
 		print(f'Non-Sensitive Document Payload: {val_input}\n')
 		print(f'Probability that document is sensitive: {int(res[0][0]*100)}%')
 		####################################################
 		
 		print('~'*80)		
 		val_input = ''' {"resource":"localhost","location":"unknown","workflow":"unauthorized"} '''		
-		res = prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len)		
+		res = prediction(loaded_model, 
+				 val_input, 
+				 to_tokenize,
+				 tokenizer, 
+				 payload_len)		
 		print(f'Non-Sensitive Document Payload: {val_input}\n')
 		print(f'Probability that document is sensitive: {int(res[0][0]*100)}%')
 		####################################################
 		
 		print('~'*80)		
 		val_input = ''' {"resource":"localhost","location":"unknown","workflow":"authorized"} '''		
-		res = prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len)		
+		res = prediction(loaded_model, 
+				 val_input, 
+				 to_tokenize, 
+				 tokenizer, 
+				 payload_len)		
 		print(f'Non-Sensitive Document Payload: {val_input}\n')
 		print(f'Probability that document is sensitive: {int(res[0][0]*100)}%')
 		####################################################
 		
 		print('~'*80)		
 		val_input = ''' {"resource":"gHOSkiQF","location":"unknown","workflow":"sfYeowi09"} '''		
-		res = prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len)		
+		res = prediction(loaded_model, 
+				 val_input, 
+				 to_tokenize, 
+				 tokenizer, 
+				 payload_len)		
 		print(f'Non-Sensitive Document Payload: {val_input}\n')
 		print(f'Probability that document is sensitive: {int(res[0][0]*100)}%')
 		####################################################
 		
 		print('~'*80)		
 		val_input = ''' {"resource":"gHOSkiQF","location":"tvHqbbjY","workflow":"sfYeowi09"} '''		
-		res = prediction(loaded_model, val_input, to_tokenize, tokenizer, payload_len)		
+		res = prediction(loaded_model, 
+				 val_input, 
+				 to_tokenize,
+				 tokenizer, 
+				 payload_len)		
 		print(f'Non-Sensitive Document Payload: {val_input}\n')
 		print(f'Probability that document is sensitive: {int(res[0][0]*100)}%')
 		
